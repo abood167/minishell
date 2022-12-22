55004ad82fa2e0a1a525ab5316235ba38c0ab8d8
Abdulrahman Bin saad <abin-saa@lab1r5s7.42abudhabi.ae>
2022-12-22 16:48:03 +0400

    gg

--- leak.sh
+++ leak.sh
@@ -1,1 +1,1 @@
-valgrind -s --track-fds=yes --tool=memcheck --leak-check=full --show-leak-kinds=all --suppressions=.ignore_readline.txt ./minishell
+valgrind -s --track-fds=yes --tool=memcheck  --trace-children=yes --leak-check=full --show-leak-kinds=all --suppressions=.ignore_readline.txt ./minishell
